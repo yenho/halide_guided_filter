1210ee7f03619a59dcf866ee80535030e131c23d
Champ Yen <champ.yen@gmail.com>
2019-08-02 21:34:33 +0800

    clean JIT code

--- gf_jit.cpp
+++ gf_jit.cpp
@@ -15,8 +15,7 @@ int main(int argc, char **argv) {
     Buffer<uint8_t> input_I = load_image(argv[1]);
     Buffer<uint8_t> input_P = load_image(argv[2]);
     int rad = atoi(argv[3]);
-    int eps = atoi(argv[4]);
-
+    int eps = atof(argv[4])*65536;
 
     Func in_I = BoundaryConditions::repeat_edge(input_I);
     Func in_P = BoundaryConditions::repeat_edge(input_P);
@@ -25,43 +24,6 @@ int main(int argc, char **argv) {
     RDom rbox(-rad, 2*rad+1, -rad, 2*rad+1);
     Expr area = u16((rad*2+1)*(rad*2+1));
 
-#if 0
-    Func down4_I, down4_P, a_b, mean_ab, mean_a, mean_b, up_mean_a, up_mean_b;
-    Func sums_x, mean_ab_x;
-
-    Expr val_I = f32(in_I(x + r, y));
-    Expr val_P = f32(in_P(x + r, y));
-    Expr mean_I = sum(val_I);
-    Expr mean_P = sum(val_P);
-    Expr corr_I = sum(val_I*val_I);
-    Expr corr_IP = sum(val_I*val_P);
-    sums_x(x, y) = Tuple(mean_I, mean_P, corr_I, corr_IP);
-    Tuple sums = Tuple(
-                        sum(sums_x(x, y+r)[0])/area, 
-                        sum(sums_x(x, y+r)[1])/area, 
-                        sum(sums_x(x, y+r)[2])/area, 
-                        sum(sums_x(x, y+r)[3])/area
-                    );
-    Expr var_I = sums[2] - sums[0]*sums[0];
-    Expr cov_IP = sums[3] - sums[0]*sums[1];
-
-    Expr a = cov_IP/(var_I + eps);
-    Expr b = sums[1] - a*sums[0];
-
-    a_b(x, y) = Tuple(a, b);
-
-    mean_ab_x(x, y) = Tuple(
-                        sum(a_b(x + r, y)[0]), 
-                        sum(a_b(x + r, y)[1])
-                    );
-    mean_ab(x, y) = Tuple(
-                        sum(mean_ab_x(x, y + r)[0])/area, 
-                        sum(mean_ab_x(x, y + r)[1])/area
-                    );
-    Func output;
-    output(x, y) = u8(mean_ab(x, y)[0]*f32(in_I(x, y)) + mean_ab(x, y)[1]);
-    //output(x, y) = u8(a_b(x, y)[0]*256);
-#else
     Func sums_x, a_b, mean_ab, mean_ab_x, output;
 
     Expr val_I = u16(in_I(x+r, y));
@@ -89,7 +51,6 @@ int main(int argc, char **argv) {
     mean_ab(x, y) = Tuple( sum(mean_ab_x(x, y+r)[0])/area, i16(sum(mean_ab_x(x, y+r)[1])/area) );
     output(x, y) = u8((mean_ab(x, y)[0]*in_I(x, y) + mean_ab(x, y)[1]) >> 7);
 
-#endif
     Buffer<uint8_t> result = output.realize(input_I.width(), input_I.height());
     save_image(result, argv[5]);
     return 0;
